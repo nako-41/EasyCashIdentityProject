60418fad4cdb5e4193ea5721b7c3d318952a4aad
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-07-28 17:06:13 +0300

    context ve identity iliskileri kuruldu

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext<AppUser,AppRole,int>
+    {   
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=OKANK;initial catalog=EasyCashDB;integrated Security=true");
+        }
+        public DbSet<CustomerAccount> customerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> customerAccountProcesses { get; set; }
+
+
+
+    }
+}
---
+++ DataAccessLayer/Migrations/20230728140102_mig_add_relation_customeraccount_appuser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig_add_relation_customeraccount_appuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppUserID",
+                table: "customerAccounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customerAccounts_AppUserID",
+                table: "customerAccounts",
+                column: "AppUserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_customerAccounts_AspNetUsers_AppUserID",
+                table: "customerAccounts",
+                column: "AppUserID",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_customerAccounts_AspNetUsers_AppUserID",
+                table: "customerAccounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_customerAccounts_AppUserID",
+                table: "customerAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserID",
+                table: "customerAccounts");
+        }
+    }
+}
