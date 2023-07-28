6eba76c1e500b4e95650b1d4fc09137d97b837d0
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-07-29 01:14:08 +0300

    business katmani yavasdan kuruldu

--- EntityLayer/Concrete/AppUser.cs
+++ EntityLayer/Concrete/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,18 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class AppUser:IdentityUser<int>
+    public class AppUser : IdentityUser<int>
     {
+   
+
+        //public string Id => throw new NotImplementedException();
+
         public string Name { get; set; }
         public string Surname { get; set; }
         public string District { get; set; }
         public string City { get; set; }
         public string ImageUrl { get; set; }
         public List<CustomerAccount> customerAccounts { get; set; }
-
+        //public string UserName { get ; set ; }
     }
 }
---
+++ UILayer/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using DtoLayer.Dtos.AppUserDto;
+using EntityLayer.Concrete;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //private readonly UserManager<AppUser> _userManager;
+        //public RegisterController(UserManager<AppUser> userManager)
+        //{
+        //    _userManager = userManager;
+        ////}
+        //UserManager<AppUser> _userManager = new UserManager<AppUser>();
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        AppUser appUser = new AppUser()
+        //        {
+        //            UserName = appUserRegisterDto.Username,
+        //            Name= appUserRegisterDto.Name,
+        //            Surname = appUserRegisterDto.Surname,
+        //            Email = appUserRegisterDto.Email,
+        //        };
+        //        var result=await _userManager.CreateAsync(appUser,appUserRegisterDto.Password);
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("Index", "ConfirmMail");
+        //        }
+        //        return View();
+
+        //    }
+
+        //    return View();
+        //}
+
+    }
+}
