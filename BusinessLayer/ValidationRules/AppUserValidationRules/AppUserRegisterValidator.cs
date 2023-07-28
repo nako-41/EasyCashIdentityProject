ad19fed1b0d377d3a11b3cc3c2a06f6cf0ee4af1
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-07-28 17:28:57 +0300

    fluent validation dto ayarlamarı yapıldı

---
+++ BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using DtoLayer.Dtos.AppUserDto;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı bos gecilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı bos gecilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanici alanı bos gecilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı bos gecilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Sifre alanı bos gecilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Sifre tekrar alanı bos gecilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("lutfen en fazla 30 karakter girisi yapiniz");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("lutfen en az 3 karakter girisi yapiniz");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("parolaniz eslesmiyor");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("lutfen gecerli bir mail adresi giriniz");
+        }
+    }
+}
--- DtoLayer/Dtos/AppUserDto/AppUserRegisterDto.cs
+++ DtoLayer/Dtos/AppUserDto/AppUserRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,17 @@ namespace DtoLayer.Dtos.AppUserDto
 {
     public class AppUserRegisterDto
     {
-
+        //[Required(ErrorMessage ="name is required")]
+        //[Display(Name="Isim :")]
+        //[MaxLength(30)]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
 
     }
 }
+
+//ad,soyad,username,mail,password,confirmapassword
