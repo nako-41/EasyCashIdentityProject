cd5de6c3acca7c5822d0d30147d4a9bfc5d2f650
okan kaipoglu <okan_kaipoglu@outlook.com>
2023-08-02 15:51:44 +0300

    maile onay icin kod gonderme testi yapıldı,ve maile kod basarılı sekilde gitti

--- EntityLayer/Concrete/AppUser.cs
+++ EntityLayer/Concrete/AppUser.cs
@@ -19,6 +19,7 @@ namespace EntityLayer.Concrete
         public string District { get; set; }
         public string City { get; set; }
         public string ImageUrl { get; set; }
+        public int ConfirmCode { get; set; }
         public List<CustomerAccount> customerAccounts { get; set; }
         //public string UserName { get ; set ; }
     }
--- UILayer/Controllers/RegisterController.cs
+++ UILayer/Controllers/RegisterController.cs
@@ -1,8 +1,11 @@
 ﻿using DtoLayer.Dtos.AppUserDto;
 using EntityLayer.Concrete;
+using MailKit.Net.Smtp;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System.Net.Mail;
 
 namespace UILayer.Controllers
 {
@@ -26,7 +29,12 @@ namespace UILayer.Controllers
         [HttpPost]
         public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
         {
-            if (ModelState.IsValid)
+            Random random = new Random();
+
+            int code = random.Next(100000, 1000000);
+
+
+			if (ModelState.IsValid)
             {
                 AppUser appUser = new AppUser()
                 {
@@ -36,13 +44,39 @@ namespace UILayer.Controllers
                     Email = appUserRegisterDto.Email,
                     City="aa",
                     District="bb",
-                    ImageUrl="cc"
+                    ImageUrl="cc",
+                    ConfirmCode=code
                    
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
 
                 if (result.Succeeded)
                 {
+                    MimeMessage mimeMessage = new MimeMessage();
+                    MailboxAddress mailboxAddressFrom = new MailboxAddress("Easy Cash Admin", "kaipoglu267@gmail.com");
+                    MailboxAddress mailboxAddressTo = new MailboxAddress("User", appUser.Email);
+
+                    mimeMessage.From.Add(mailboxAddressFrom);
+                    mimeMessage.To.Add(mailboxAddressTo);
+
+
+                    var bodybuilder = new BodyBuilder();
+                    bodybuilder.TextBody = "Kayıt işlemini gerçekleştirmek için onay kodunuz: " + code;
+                    mimeMessage.Body=bodybuilder.ToMessageBody();
+
+                    mimeMessage.Subject = "easy cash onay kodu";
+
+                    MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient();
+                    client.Connect("smtp.gmail.com", 587, false);
+                    client.Authenticate("kaipoglu267@gmail.com", "mczvwazmpzmkctgi");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+
+
+
+
+
+
                     return RedirectToAction("Index", "ConfirmMail");
                 }
                 else
